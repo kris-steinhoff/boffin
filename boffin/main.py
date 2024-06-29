437d82990c36bf9bc500588d3360f45bb19384b6
Kris Steinhoff <ksteinhoff@gmail.com>
2024-06-29 14:11:20 -0400

    Add graphql

--- boffin/main.py
+++ boffin/main.py
@@ -1,6 +1,21 @@
+import strawberry
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 from boffin.hero.rest import router as hero_router
 
 app = FastAPI()
 app.include_router(hero_router)
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+
+schema = strawberry.Schema(Query)
+graphql_app: GraphQLRouter = GraphQLRouter(schema)
+
+app.include_router(graphql_app, prefix="/graphql")

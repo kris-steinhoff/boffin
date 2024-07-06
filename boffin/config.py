0bbcba117122b71ef3e1835f3633c03a1764e9cb
Kris Steinhoff <ksteinhoff@gmail.com>
2024-07-06 10:43:02 -0400

    Add redis cache

--- boffin/config.py
+++ boffin/config.py
@@ -1,10 +1,16 @@
-from functools import cache
+from functools import cache, cached_property
 
+import redis.asyncio as redis
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     database_url: str
+    redis_url: str
+
+    @cached_property
+    def redis_client(self) -> redis.Redis:
+        return redis.from_url(self.redis_url)
 
 
 @cache
